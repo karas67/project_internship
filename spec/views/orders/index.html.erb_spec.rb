9bb19b2e84b1c3c86905bb94f603f52995e46af2
Bazhanov Vasyl <karas060385@gmail.com>
2022-05-06 17:07:35 +0300

    git commit -m 'add view bootstrap, worked model order, homework for lesson 4'

---
+++ spec/views/orders/index.html.erb_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe "orders/index.html.erb" do
+  let(:valid_cashier1) { FactoryBot.create(:cashier) }
+  let(:valid_cashier2) { FactoryBot.create(:cashier) }
+  let(:order) { FactoryBot.create(:order, cashier_id: valid_cashier1.id) }
+  let(:order1) { FactoryBot.create(:order, cashier_id: valid_cashier2.id) }
+
+  it "Show info about user" do
+    assign(:orders, [order1, order])
+
+    render
+    expect(rendered).to match order.cashier.first_name
+    expect(rendered).to match order.date.to_s
+    expect(rendered).to match order.name
+    expect(rendered).to match order.amount.to_s
+    expect(rendered).to match order1.cashier.first_name
+    expect(rendered).to match order1.date.to_s
+    expect(rendered).to match order1.name
+    expect(rendered).to match order1.amount.to_s
+  end
+
+  it "Show link to main list" do
+    assign(:orders, [order1, order])
+
+    render
+
+    expect(rendered).to match 'New order'
+  end
+end
