8cc26428f0c4ae2aa428b39595e5cdb871c7a3de
Bazhanov Vasyl <karas060385@gmail.com>
2022-05-20 16:12:08 +0300

    update file device

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,19 @@ import 'bootstrap/js/dist/dropdown'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 
+// Import the specific modules you may need (Modal, Alert, etc)
+import { Tooltip, Popover } from "bootstrap"
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+  // Both of these are from the Bootstrap 5 docs
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+  return new Tooltip(tooltipTriggerEl)
+  })
+  
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+  return new Popover(popoverTriggerEl)
+  })
+})
