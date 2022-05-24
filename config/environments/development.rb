69a1676aa7f0bf89157b74907a6e48c061164086
Bazhanov Vasyl <karas060385@gmail.com>
2022-05-24 23:25:34 +0300

    add email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,13 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  #config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.default_url_options = { host: 'afternoon-dusk-26679.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+  
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
-
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
