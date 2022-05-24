708b89eb19045c4aeba46ced8fbaedf7e769a624
Bazhanov Vasyl <karas060385@gmail.com>
2022-05-24 22:43:28 +0300

    add email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,13 +34,25 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'afternoon-dusk-26679.herokuapp.com',
+    user_name:            'karas060385@gmail.com',
+    password:             'jnhpzggzhnvwpgeo',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
