5abd920ee2a4f316dbad948dd9518368ed26ef2e
Bazhanov Vasyl <karas060385@gmail.com>
2022-05-25 22:37:43 +0300

    update db

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,16 @@ module ProjectInternship
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => "xxxxx@gmail.com",
+      :password => "xxxxxx",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
   end
 end
